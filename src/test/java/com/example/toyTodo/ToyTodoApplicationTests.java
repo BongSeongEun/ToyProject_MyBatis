0650296e4de70e191fe08c7626c3facc2be772ed
BongSeongEun <bongfam2@naver.com>
2025-04-23 15:20:53 +0900

    feat1: Spring Setting

---
+++ src/test/java/com/example/toyTodo/ToyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
